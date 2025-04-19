a57b93f16e4df8bbd509912210aaa10a02589254
Uhama91 <uhama@MSI.>
2025-04-19 12:12:47 +0200

    done the first TDD TOP's ex

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const time = Math.floor(Math.random()*1001)
 
-};
+const repeatString = function(str, time) {
+    if(typeof str !== "string") return false;
+    
+    if(time < 0) return "ERROR";
+
+    if(str === '') return '';
+
+    let newStr = "";
+    for(let i = 0; i < time; i++){
+        if(time === 0) newStr === " ";
+        newStr += str;
+    }
+
+    return newStr;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
