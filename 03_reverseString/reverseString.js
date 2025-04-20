01365bc0bac844b69b5c8fa309095f07e028870b
Uhama91 <uhama@MSI.>
2025-04-20 07:38:44 +0200

    done the third TDD ex from TOP about reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverseStr = "";
+    let arrayStr = str.split('');
+    const length = arrayStr.length;
+    
+    for(let i=0; i<length; i++){
+        reverseStr += arrayStr.pop();
+    }
 
+    return reverseStr;
 };
 
 // Do not edit below this line
